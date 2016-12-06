98fca2364bdb3b746171b500fa8e57adb1c3c067
NadanKim <paintime@naver.com>
2016-12-06 19:23:27 +0900

    26stage and 27 stage added

--- BubbleBobble/STAGE.py
+++ BubbleBobble/STAGE.py
@@ -27,7 +27,7 @@ class STAGE:
     musics = []
     sounds = None
     def __init__(self):
-        self.currentStage = 24
+        self.currentStage = 26
         self.stageMoveCount = 0.0
         self.stageSize = 25
         self.tileSize = 12.5
@@ -230,7 +230,7 @@ class STAGE:
         if self.hurry:
             self.hurry = False
             self.musics[0].repeat_play()
-        if self.currentStage == 26:
+        if self.currentStage == 28:
             self.currentStage = 100
             self.musics[2].play()
         elif self.currentStage == 101:
